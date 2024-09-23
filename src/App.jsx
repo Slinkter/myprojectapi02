278cac5c85e43237a8ac029d848944cc299b3ea2
Slinkter <luis.j.cueva@gmail.com>
2024-09-23 12:14:44 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// Importar React y varios hooks de React
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useMemo, memo } from "react";
 import {
     Input,
     Card,
@@ -7,146 +6,130 @@ import {
     Typography,
     Spinner,
 } from "@material-tailwind/react";
-// Importar funciones asincrónicas para obtener datos de la API
 import { getUser } from "./components/getUserApi";
 import { getPost } from "./components/getPostApi";
-// Componente funcional
+
 const App = () => {
-    // hook : Estados locales
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
     const [numberId, setNumberId] = useState(1);
     const [user, setUser] = useState({});
     const [post, setPost] = useState([]);
-    // Función
-    const updateUser = useCallback(() => {
-        console.group("Func 1 - updateUser");
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(null);
 
+    // Función para obtener el usuario
+    const fetchUser = useCallback(async () => {
+        setIsLoading(true);
         try {
-            setIsLoading(true);
-            getUser(numberId)
-                .then((data) => setUser(data)) // Actualizar el estado 'user' con los datos obtenidos
-                .catch((err) => setIsError(err)); // Manejar errores si la solicitud falla
-        } catch (error) {
-            setIsError(error);
-            console.log("error : ", error);
+            const data = await getUser(numberId);
+            setUser(data);
+        } catch (err) {
+            setIsError("Error fetching user");
         } finally {
             setIsLoading(false);
         }
-        console.log(user);
-        console.groupEnd();
-    }, [numberId]); // Dependencia: 'numberId'
+    }, [numberId]);
 
-    // Función
-    const updatePost = useCallback(() => {
-        console.group("Func 2 - updateUser");
+    // Función para obtener los posts
+    const fetchPost = useCallback(async () => {
         try {
-            getPost(user?.id)
-                .then((data) => setPost(data)) // Actualizar el estado 'post' con los datos obtenidos
-                .catch((err) => setIsError(err)); // Manejar errores si la solicitud falla
-        } catch (error) {
-            setIsError(error);
-            console.log("error : ", error);
-        } finally {
-            setIsLoading(false);
+            const data = await getPost(user.id);
+            setPost(data);
+        } catch (err) {
+            setIsError("Error fetching posts");
         }
-        console.log(post);
-        console.groupEnd();
-    }, [user.id]); // Dependencia: 'user.id'
+    }, [user.id]);
 
-    // Efecto para actualizar datos de usuario cuando 'numberId' cambia
     useEffect(() => {
-        console.log("useEffect - 1");
-        updateUser();
-    }, [numberId, updateUser]); // Dependencias: 'numberId', 'updateUser'
+        fetchUser();
+    }, [numberId, fetchUser]);
 
-    // Efecto para actualizar datos de posts cuando 'user' cambia
     useEffect(() => {
-        console.log("useEffect - 2");
-        if (!user.id) {
-            setPost([]); // Si 'user.id' es null, establecer 'post' como un array vacío
-            // setNumberId(0);
-        } else {
-            updatePost(); // Si 'user.id' es truthy, obtener y actualizar datos de posts
-        }
-    }, [user, updatePost]); // Dependencias: 'user', 'updatePost'
+        if (user.id) fetchPost();
+        else setPost([]);
+    }, [user, fetchPost]);
 
-    console.group("Render Component");
-    console.log("render");
-    console.log("numberId : ", numberId);
-    console.log("user : ", user);
-    console.log("post : ", post);
-    console.groupEnd();
+    // Memorizar la lista de posts
+    const postList = useMemo(
+        () =>
+            post.map((item, idx) => (
+                <Typography key={item.id} variant="par">
+                    <span>{idx}</span>) {item.title}.
+                </Typography>
+            )),
+        [post]
+    );
 
     return (
         <div className="container-custom">
-            <Typography variant="h1" color="blue-gray">
-                Fake API
-            </Typography>
-            <Typography variant="lead" color="blue-gray">
-                jsonplaceholder + tailwind css
-            </Typography>
-            {/* Condición de renderizado basada en 'user.id' */}
-            {isLoading && <Spinner className="h-36 w-36 " />}
-            {!isLoading && (
-                <div className="w-3/4 text-center ">
-                    <Input
-                        size="lg"
-                        color="blue"
-                        label="Entre [1-10]"
-                        name="numberId"
-                        value={numberId}
-                        onChange={(e) => setNumberId(Number(e.target.value))}
-                    />
+            {isLoading ? (
+                <Spinner className="h-24 w-24" />
+            ) : isError ? (
+                <div className="error-message">
+                    An error occurred: {isError}
                 </div>
-            )}
-            {!isLoading && (
-                <div className="flex flex-col justify-around items-center  m-0 p-0 gap-6 text-center  w-3/4  ">
-                    {numberId === 0 || numberId > 10 || numberId < 0 ? (
-                        <>
-                            <Card className="mt-2 w-full">
-                                <CardBody>
-                                    <Typography variant="h5" color="red">
-                                        no existe usuario
-                                    </Typography>
-                                    <Typography variant="lead" color="red">
-                                        debes escoger entre [1-10]
-                                    </Typography>
-                                </CardBody>
-                            </Card>
-                        </>
-                    ) : (
-                        <div className="flex flex-col justify-center gap-6 w-full">
-                            <Card className="mt-2 w-full">
-                                <CardBody>
-                                    <Typography>{user.name}</Typography>
-                                    <Typography>{user.email}</Typography>
-                                    <Typography>{user.phone}</Typography>
-                                </CardBody>
-                            </Card>
+            ) : (
+                <>
+                    <Typography variant="h1" color="blue-gray">
+                        Fake API
+                    </Typography>
+                    <Typography variant="lead" color="blue-gray">
+                        jsonplaceholder + tailwind css
+                    </Typography>
+                    <div className="w-3/4 text-center ">
+                        <Input
+                            size="lg"
+                            color="blue"
+                            label="Entre [1-10]"
+                            name="numberId"
+                            value={numberId}
+                            onChange={(e) =>
+                                setNumberId(Number(e.target.value))
+                            }
+                        />
+                    </div>
 
-                            {/* Renderizar lista de posts */}
-                            <Card className="w-full">
-                                <CardBody>
-                                    <ul className="text-left">
-                                        {post.map((item, idx) => (
-                                            <Typography
-                                                key={item.id}
-                                                variant="par"
-                                            >
-                                                <span>{idx}</span> ){" "}
-                                                {item.title}.
-                                            </Typography>
-                                        ))}
-                                    </ul>
-                                </CardBody>
-                            </Card>
+                    {numberId <= 0 || numberId > 10 ? (
+                        <Card className="mt-2 w-3/4">
+                            <CardBody>
+                                <Typography variant="h5" color="red">
+                                    No existe usuario
+                                </Typography>
+                                <Typography variant="lead" color="red">
+                                    Debes escoger entre [1-10]
+                                </Typography>
+                            </CardBody>
+                        </Card>
+                    ) : (
+                        <div className="flex flex-col justify-center gap-6 w-3/4">
+                            <CardUser user={user} />
+                            <CardPostUser postList={postList} />
                         </div>
                     )}
-                </div>
+                </>
             )}
         </div>
     );
 };
+
+const CardPostUser = ({ postList }) => (
+    <Card className="w-full">
+        <CardBody>
+            <ul className="text-left">{postList}</ul>
+        </CardBody>
+    </Card>
+);
+
+// Componente memoizado para evitar renderizados innecesarios
+const CardUser = memo(({ user }) => (
+    <Card className="mt-2 w-full">
+        <CardBody>
+            <Typography>{user.name}</Typography>
+            <Typography>{user.email}</Typography>
+            <Typography>{user.phone}</Typography>
+        </CardBody>
+    </Card>
+));
+
+CardUser.displayName = "CardUser";
 
 export default App;
--- src/components/getPostApi.js
+++ src/components/getPostApi.js
@@ -1,6 +1,11 @@
 const getPost = async (userId) => {
-    const url = "https://jsonplaceholder.typicode.com/posts?userId=" + userId;
+    const url = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
+
     const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Network response was not ok: ${res.statusText}`);
+    }
+
     const data = await res.json();
     return data; // [{},{}]
 };
